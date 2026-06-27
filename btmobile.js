77192e5752f41fff236dbdfd5c12e44015bf1806
Guilherme Silva <guih.bittencourt23@gmail.com>
2026-06-27 18:52:31 -0300

    update de seo

--- btmobile.js
+++ btmobile.js
@@ -1,17 +1,34 @@
-/*BOTAO MOBILE ONCLICK*/
+/* BOTAO MOBILE ONCLICK */
+// Seleciona os elementos do botão de abrir, do menu e do overlay (fundo escuro)
 let button = document.getElementById("btn");
 let menu = document.getElementById("menu-mobile");
 let overlay = document.getElementById("overlay-menu");
 
+// Evento que abre o menu mobile ao clicar no botão
 button.addEventListener("click", () => {
     menu.classList.add("abrir-menu")
 })
+
+// Evento que fecha o menu mobile ao clicar em qualquer item dentro do próprio menu
 menu.addEventListener("click", () => {
     menu.classList.remove("abrir-menu")
 })
+
+// Evento que fecha o menu mobile ao clicar na área escura (overlay)
 overlay.addEventListener("click", () => {
     menu.classList.remove("abrir-menu")
 })
+
+// Evento global para fechar o menu ao clicar fora dele
+document.addEventListener("click", (event) => {
+    // Verifica se o menu está aberto, se o clique não foi dentro do menu e nem no botão de abrir
+    // target Indica o elemento real que disparou o evento (clicado)
+    //contains = contém / ! = não
+    if (menu.classList.contains("abrir-menu") && !menu.contains(event.target) && !button.contains(event.target)) {
+        menu.classList.remove("abrir-menu");
+    }
+})
+
 
 /*menu topo*/
 document.addEventListener("DOMContentLoaded", function () {
